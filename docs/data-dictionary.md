34d815561b956584d740c3f68eac596e217c24ca
Fer Aguirre <kay_op@protonmail.com>
2022-07-08 14:12:29 -0500

    Initial commit

---
+++ docs/data-dictionary.md
@@ -0,0 +1,1 @@
+# Monitor Gender Based Violence
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Monitor Gender Based Violence',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Monitoring gender-based violence on Twitter.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
